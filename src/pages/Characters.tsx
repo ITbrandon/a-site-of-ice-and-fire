f4e5e77adc734f1deacba88f4a53e099c6b34f63
ITbrandon <brandoniticka@gmail.com>
2025-04-01 13:39:56 -0600

    fixed filter feature

--- src/pages/Characters.tsx
+++ src/pages/Characters.tsx
@@ -23,7 +23,7 @@ type Character = {
 const [filter, setFilter] = useState<string>(localStorage.getItem('filter') || "")
 
 const characters = useLoaderData() as Character[]
-const displayedCharacters = filter ? characters.filter(char => char.fullName.includes(filter)) : characters
+const displayedCharacters = filter ? characters.filter(char => char.fullName.toLowerCase().trim().includes(filter.toLowerCase().trim())) : characters
 
 function filterCharacters(event: React.ChangeEvent<HTMLInputElement>) {
   setFilter(event.target.value)
@@ -34,7 +34,9 @@ function filterCharacters(event: React.ChangeEvent<HTMLInputElement>) {
     <div className="bg-white">
       <div className="mx-auto max-w-2xl px-4 py-16 sm:px-6 sm:py-24 lg:max-w-7xl lg:px-8">
         <h2 className="sr-only">Characters</h2>
+        <div className="mb-2">
         <input type="text" placeholder="Look up a character" className="block w-full rounded-md bg-white px-3 py-1.5 text-base text-gray-900 outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline-2 focus:-outline-offset-2 focus:outline-black sm:text-sm/6" onChange={filterCharacters}/>
+        </div>
         <div className="grid grid-cols-1 gap-x-6 gap-y-10 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 xl:gap-x-8">
           {displayedCharacters.map((char: Character) => (
             <Link key={char._id} to={`/characters/${char._id}`} className="group">
