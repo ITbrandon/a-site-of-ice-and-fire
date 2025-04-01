35177e92cd76367997a4ddb0a8973a74ad0a7a46
ITbrandon <brandoniticka@gmail.com>
2025-04-01 13:49:35 -0600

    final fix in search

--- src/pages/Characters.tsx
+++ src/pages/Characters.tsx
@@ -34,11 +34,11 @@ function filterCharacters(event: React.ChangeEvent<HTMLInputElement>) {
     <div className="bg-white">
       <div className="mx-auto max-w-2xl px-4 py-16 sm:px-6 sm:py-24 lg:max-w-7xl lg:px-8">
         <h2 className="sr-only">Characters</h2>
-        <div className="mb-2">
+        <div className="mb-4">
         <input type="text" placeholder="Look up a character" className="block w-full rounded-md bg-white px-3 py-1.5 text-base text-gray-900 outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline-2 focus:-outline-offset-2 focus:outline-black sm:text-sm/6" onChange={filterCharacters}/>
         </div>
         <div className="grid grid-cols-1 gap-x-6 gap-y-10 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 xl:gap-x-8">
-          {displayedCharacters.map((char: Character) => (
+          {displayedCharacters.length > 0 ? displayedCharacters.map((char: Character) => (
             <Link key={char._id} to={`/characters/${char._id}`} className="group">
               <img
                 alt={char.fullName}
@@ -48,7 +48,7 @@ function filterCharacters(event: React.ChangeEvent<HTMLInputElement>) {
               <h3 className="mt-4 text-sm text-gray-700">{char.family}</h3>
               <p className="mt-1 text-lg font-medium text-gray-900">{char.fullName}</p>
             </Link>
-          ))}
+          )) : <h1>No Characters Match Your Search</h1>}
         </div>
       </div>
     </div>
