fccb7e4b757e5a4a7865ea556029dc534b3a3aa9
ITbrandon <brandoniticka@gmail.com>
2025-04-01 13:27:14 -0600

    added a character search feature

--- src/pages/Characters.tsx
+++ src/pages/Characters.tsx
@@ -1,3 +1,4 @@
+import { useState } from "react";
 import { useLoaderData } from "react-router-dom";
 import { Link } from "react-router-dom";
 import { getCharacters } from "../server/api";
@@ -19,15 +20,23 @@ type Character = {
   title: string
 }
 
+const [filter, setFilter] = useState<string>(localStorage.getItem('filter') || "")
+
 const characters = useLoaderData() as Character[]
+const displayedCharacters = filter ? characters.filter(char => char.fullName.includes(filter)) : characters
+
+function filterCharacters(event: React.ChangeEvent<HTMLInputElement>) {
+  setFilter(event.target.value)
+  localStorage.setItem('filter', event.target.value)
+}
 
   return (
     <div className="bg-white">
       <div className="mx-auto max-w-2xl px-4 py-16 sm:px-6 sm:py-24 lg:max-w-7xl lg:px-8">
         <h2 className="sr-only">Characters</h2>
-
+        <input type="text" placeholder="Look up a character" className="block w-full rounded-md bg-white px-3 py-1.5 text-base text-gray-900 outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline-2 focus:-outline-offset-2 focus:outline-black sm:text-sm/6" onChange={filterCharacters}/>
         <div className="grid grid-cols-1 gap-x-6 gap-y-10 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 xl:gap-x-8">
-          {characters.map((char: Character) => (
+          {displayedCharacters.map((char: Character) => (
             <Link key={char._id} to={`/characters/${char._id}`} className="group">
               <img
                 alt={char.fullName}
